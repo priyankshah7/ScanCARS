7d028a524e518ff21d3ff38e274fac6c6327cf06
Priyank Shah <priyank.shah@kcl.ac.uk>
2017-12-21 01:53:56 +0000

    Updated ui thread.
    Signed-off-by: Priyank Shah <priyank.shah@kcl.ac.uk>

--- scancars/threads/uithread.py
+++ scancars/threads/uithread.py
@@ -1,12 +1,11 @@
-import sys, time, ctypes
+import time
 import numpy as np
 from PyQt5 import QtCore
 
-from scancars import main
-from scancars.sdk import andor
-from scancars.utils.toggle import Toggle
+from scancars.sdk.andor import Cam
+from scancars.utils import toggle, post
 
-toggle = Toggle()
+andor = Cam()
 
 # TODO Check to see if pyqtSlot can be used for method-only instead of within class using the run method
 # TODO Try called the stop method directly when stopping a signal instead of sending a signal to it.
@@ -16,53 +15,57 @@ class Signals(QtCore.QObject):
     finished = QtCore.pyqtSignal()
 
 
-class Initialize(QtCore.QRunnable, main.ScanCARS):
-    def __init__(self, parent=main.ScanCARS):
-        super(Initialize, self).__init__(parent)
+class Initialize(QtCore.QRunnable):
+    def __init__(self, ui):
+        super(Initialize, self).__init__()
         self.signals = Signals()
+        self.ui = ui
 
     @QtCore.pyqtSlot()
     def run(self):
-        toggle.deactivate_buttons()
+        toggle.deactivate_buttons(self.ui)
 
-        randtrack = np.array([int(self.CameraOptions_track1lower.text()),
-                              int(self.CameraOptions_track1upper.text()),
-                              int(self.CameraOptions_track2lower.text()),
-                              int(self.CameraOptions_track2upper.text())])
+        randtrack = np.array([int(self.ui.CameraOptions_track1lower.text()),
+                              int(self.ui.CameraOptions_track1upper.text()),
+                              int(self.ui.CameraOptions_track2lower.text()),
+                              int(self.ui.CameraOptions_track2upper.text())])
 
-        andor.initialize()
-        _, width = andor.getdetector()
+        errorinitialize = andor.initialize()
+        if errorinitialize != 'DRV_SUCCESS':
+            post.eventlog(self.ui, 'Andor: Initialize error. ' + errorinitialize)
+            return
+        andor.getdetector()
         andor.setshutter(1, 2, 0, 0)
         andor.setreadmode(2)
         andor.setrandomtracks(2, randtrack)
         andor.setadchannel(1)
         andor.settriggermode(0)
         andor.sethsspeed(1, 0)
-        andor.setvsspeed(3)
+        andor.setvsspeed(4)
 
-        toggle.activate_buttons()
+        andor.dim = andor.width * andor.randomtracks
+
+        toggle.activate_buttons(self.ui)
         self.signals.finished.emit()
 
 
-class Acquire(QtCore.QRunnable):
-    def __init__(self):
-        super(Acquire, self).__init__()
+class Shutdown(QtCore.QRunnable):
+    def __init__(self, ui):
+        super(Shutdown, self).__init__()
         self.signals = Signals()
-
-    @QtCore.pyqtSlot()
-    def stop(self):
-        pass
+        self.ui = ui
 
     @QtCore.pyqtSlot()
     def run(self):
-        pass
+        andor.setshutter(1, 2, 0, 0)
+        andor.iscooleron()
+        if andor.coolerstatus == 0:
+            andor.shutdown()
 
+        elif andor.coolerstatus == 1:
+            andor.cooleroff()
+            while andor.temperature < -20:
+                time.sleep(1)
+            andor.shutdown()
 
-class CameraTemp(QtCore.QRunnable):
-    def __init__(self):
-        super(CameraTemp, self).__init__()
-        self.signals = Signals()
-
-    @QtCore.pyqtSlot()
-    def run(self):
-        pass
+        self.signals.finished.emit()
