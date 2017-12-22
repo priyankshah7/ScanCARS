54c9d5ab94c244eb60f4802f930b3542fe6ca88c
Priyank Shah <priyank.shah@kcl.ac.uk>
2017-12-22 18:12:43 +0000

    Enable individual pushbuttons to counter main activate/deactivate method call.
    Signed-off-by: Priyank Shah <priyank.shah@kcl.ac.uk>

--- scancars/utils/toggle.py
+++ scancars/utils/toggle.py
@@ -1,29 +1,46 @@
-# Functions to activate or deactivate all the buttons in the GUI
+# Functions to activate/deactivate pushbuttons in the main GUI
+"""
+All pushbuttons are, by default, set to be activated/deactivated depending on which method is called.
+However individual buttons can be set to be countered to the main call (e.g. activated despite call
+for deactivate_buttons() by doing the following:
+
+e.g. deactivate all buttons apart from the main Shutdown pushbutton:
+
+    deactivate_buttons(self, main_shutdown_stat=True)
+"""
 
 
-def deactivate_buttons(self):
-    self.Main_start_acq.setEnabled(False)
-    self.Main_shutdown.setEnabled(False)
-    self.CameraTemp_cooler_on.setEnabled(False)
-    self.SpectraWin_sum_track.setEnabled(False)
-    self.SpectraWin_single_track.setEnabled(False)
-    self.Grating_update.setEnabled(False)
-    self.CameraOptions_openimage.setEnabled(False)
-    self.CameraOptions_update.setEnabled(False)
-    self.SpectralAcq_start.setEnabled(False)
-    self.SpectralAcq_update_time.setEnabled(False)
-    self.HyperAcq_start.setEnabled(False)
+def deactivate_buttons(self, main_start_acq_stat=False, main_shutdown_stat=False, cameratemp_cooler_stat=False,
+                       spectrawin_sum_stat=False, spectrawin_single_stat=False, grating_update_stat=False,
+                       cameraoptions_openimage_stat=False, cameraoptions_update_stat=False,
+                       spectralacq_start_stat=False, spectralacq_update_stat=False, hyperacq_start_stat=False):
+
+    self.Main_start_acq.setEnabled(main_start_acq_stat)
+    self.Main_shutdown.setEnabled(main_shutdown_stat)
+    self.CameraTemp_cooler_on.setEnabled(cameratemp_cooler_stat)
+    self.SpectraWin_sum_track.setEnabled(spectrawin_sum_stat)
+    self.SpectraWin_single_track.setEnabled(spectrawin_single_stat)
+    self.Grating_update.setEnabled(grating_update_stat)
+    self.CameraOptions_openimage.setEnabled(cameraoptions_openimage_stat)
+    self.CameraOptions_update.setEnabled(cameraoptions_update_stat)
+    self.SpectralAcq_start.setEnabled(spectralacq_start_stat)
+    self.SpectralAcq_update_time.setEnabled(spectralacq_update_stat)
+    self.HyperAcq_start.setEnabled(hyperacq_start_stat)
 
 
-def activate_buttons(self):
-    self.Main_start_acq.setEnabled(True)
-    self.Main_shutdown.setEnabled(True)
-    self.CameraTemp_cooler_on.setEnabled(True)
-    self.SpectraWin_sum_track.setEnabled(True)
-    self.SpectraWin_single_track.setEnabled(True)
-    self.Grating_update.setEnabled(True)
-    self.CameraOptions_openimage.setEnabled(True)
-    self.CameraOptions_update.setEnabled(True)
-    self.SpectralAcq_start.setEnabled(True)
-    self.SpectralAcq_update_time.setEnabled(True)
-    self.HyperAcq_start.setEnabled(True)
+def activate_buttons(self, main_start_acq_stat=True, main_shutdown_stat=True, cameratemp_cooler_stat=True,
+                     spectrawin_sum_stat=True, spectrawin_single_stat=True, grating_update_stat=True,
+                     cameraoptions_openimage_stat=True, cameraoptions_update_stat=True,
+                     spectralacq_start_stat=True, spectralacq_update_stat=True, hyperacq_start_stat=True):
+
+    self.Main_start_acq.setEnabled(main_start_acq_stat)
+    self.Main_shutdown.setEnabled(main_shutdown_stat)
+    self.CameraTemp_cooler_on.setEnabled(cameratemp_cooler_stat)
+    self.SpectraWin_sum_track.setEnabled(spectrawin_sum_stat)
+    self.SpectraWin_single_track.setEnabled(spectrawin_single_stat)
+    self.Grating_update.setEnabled(grating_update_stat)
+    self.CameraOptions_openimage.setEnabled(cameraoptions_openimage_stat)
+    self.CameraOptions_update.setEnabled(cameraoptions_update_stat)
+    self.SpectralAcq_start.setEnabled(spectralacq_start_stat)
+    self.SpectralAcq_update_time.setEnabled(spectralacq_update_stat)
+    self.HyperAcq_start.setEnabled(hyperacq_start_stat)
