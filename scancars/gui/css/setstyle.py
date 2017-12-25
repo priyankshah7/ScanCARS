7571d7e01c3b3ab4b482f8796dc327163e80f755
Priyank Shah <priyank.shah@kcl.ac.uk>
2017-12-25 02:18:58 +0000

    Restructured css directory.
    Signed-off-by: Priyank Shah <priyank.shah@kcl.ac.uk>

--- scancars/gui/css/setstyle.py
+++ scancars/gui/css/setstyle.py
@@ -1,9 +1,15 @@
 # Imports the css file and allows a PyQt app to call it
 from PyQt5 import QtCore
 
+# TODO Need to add css files for deactivated buttons etc and then add relevant methods here
 
-def setstyle(ui):
-    stylefile = QtCore.QFile('./forms/css/styletemp.css')
+
+def main(ui):
+    stylefile = QtCore.QFile('./gui/css/main.css')
     stylefile.open(QtCore.QFile.ReadOnly | QtCore.QFile.Text)
     ui.setStyleSheet(str(stylefile.readAll(), 'utf-8'))
     stylefile.close()
+
+
+def deactivate_all(ui):
+    pass
