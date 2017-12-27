ff07c853972d0502328f1927df5d4cfd93acbb08
Priyank Shah <priyank.shah@kcl.ac.uk>
2017-12-27 03:54:11 +0000

    General directory and file updates.
    Signed-off-by: Priyank Shah <priyank.shah@kcl.ac.uk>

--- scancars/gui/gui_spectracks.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QMainWindow
-
-from gui.forms import WindowSPECTRACKS
-
-
-class SPECTRACKS(QMainWindow, WindowSPECTRACKS.Ui_Dialog):
-    def __init__(self, parent=None):
-        super(SPECTRACKS, self).__init__(parent)
-        self.setupUi(self)
