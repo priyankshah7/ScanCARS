ba71063a3c9e394967262bcaf276ea5804f6ab5e
Priyank Shah <priyank.shah@kcl.ac.uk>
2017-12-27 03:52:10 +0000

    Updated GetAcquiredData.
    Signed-off-by: Priyank Shah <priyank.shah@kcl.ac.uk>

--- scancars/sdk/andor/pyandor.py
+++ scancars/sdk/andor/pyandor.py
@@ -175,12 +175,11 @@ class Cam:
         # TODO Can only be used for spectra (not for CCD tracks unless updated)
         self.imagearray = None
 
-        cimageArray = ctypes.c_int * self.dim
-        cimage = cimageArray()
+        cimagearray = ctypes.c_int * self.dim
+        cimage = cimagearray()
 
-        # error = dll.GetAcquiredData16(ctypes.pointer(cimage), self.dim)
-        # self.imagearray = np.asarray(cimage[:], dtype=np.uint16)
         error = dll.GetAcquiredData(ctypes.pointer(cimage), self.dim)
+
         self.imagearray = np.asarray(cimage[:])
         return ERROR_CODE[error]
 
