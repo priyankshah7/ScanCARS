7dbb62f3b31cd50a491fed9f5808a4be2990d777
Priyank Shah <priyank.shah@kcl.ac.uk>
2018-01-17 20:45:14 +0000

    Removed compression for now
    Signed-off-by: Priyank Shah <priyank.shah@kcl.ac.uk>

--- scancars/utils/savetofile.py
+++ scancars/utils/savetofile.py
@@ -1,6 +1,6 @@
 import h5py, time
 import numpy as np
-
+from PyQt5.QtWidgets import QFileDialog
 """
 INTERNAL
 Note that saving to .h5 will increase the size of the file compared to the custom .cars binary file.
@@ -11,13 +11,16 @@ The increase is roughly a factor of x3.6 even with compression (worthwhile sacri
 def save(data, path, acqproperties, acqtype='spectral'):
     if acqtype == 'spectral':
         # TODO Note that the below assumes that data has the form data[track1+track2, number of acqs]
-        track1 = np.mean(data[0:acqproperties.width-1, :])
-        track2 = np.mean(data[acqproperties.width:(2*acqproperties.width)-1, :])
+        track1 = data[0:acqproperties.width-1]
+        track2 = data[acqproperties.width:(2*acqproperties.width)-1]
 
         with h5py.File(path, 'w') as datafile:
             filegroup = datafile.create_group('sipcars')
-            filegroup.create_dataset('track1', data=track1, compression='gzip', compression_opts=9)
-            filegroup.create_dataset('track2', data=track2, compression='gzip', compression_opts=9)
+            # filegroup.create_dataset('track1', data=track1, compression='gzip', compression_opts=9)
+            # filegroup.create_dataset('track2', data=track2, compression='gzip', compression_opts=9)
+
+            filegroup.create_dataset('track1', data=track1)
+            filegroup.create_dataset('track2', data=track2)
 
             filegroup.attrs['Acquisition Type'] = 'Spectral'
             filegroup.attrs['Exposure Time'] = acqproperties.time
@@ -41,4 +44,9 @@ def save(data, path, acqproperties, acqtype='spectral'):
             filegroup.attrs['Z Pixels'] = acqproperties.zpixels
             filegroup.attrs['Time and Date'] = str(
                 time.strftime('%d/%m/%Y') + ':' + time.strftime("%H:%M:%S"))
+
+
+class EmptyClass:
+    def __init__(self):
+        pass
 
