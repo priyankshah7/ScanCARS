b492d9ce1e0a59c22769a00a1679093bcd8efbd0
Priyank Shah <priyank.shah@kcl.ac.uk>
2018-01-21 13:46:05 +0000

    Refactored variables.
    Signed-off-by: Priyank Shah <priyank.shah@kcl.ac.uk>

--- scancars/utils/post.py
+++ scancars/utils/post.py
@@ -3,17 +3,10 @@ import time
 
 
 def status(self, message):
-    self.Main_status.setText(message)
+    self.mainStatus.setText(message)
 
 
 def eventlog(self, message):
-    self.EventLogger_box.appendPlainText('(' + time.strftime("%H:%M:%S") + ')' + ' - ' + message)
+    self.eventLogger.appendPlainText('(' + time.strftime("%H:%M:%S") + ')' + ' - ' + message)
 
 
-def event_date(self):
-    self.EventLogger_box.appendPlainText(
-        '------------------------------------------------------------------------------------')
-    self.EventLogger_box.appendPlainText(
-        ' ScanCARS Software                                    Date: ' + time.strftime('%d/%m/%Y'))
-    self.EventLogger_box.appendPlainText(
-        '------------------------------------------------------------------------------------')
